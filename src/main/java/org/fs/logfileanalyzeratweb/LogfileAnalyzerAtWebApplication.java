683a853a1a742207715e098186f1ee957198c493
Floh1983 <florian.schlosser@comline.de>
2024-08-26 12:54:20 +0200

    LogfileAnalyzerAtWebApplication

---
+++ src/main/java/org/fs/logfileanalyzeratweb/LogfileAnalyzerAtWebApplication.java
@@ -0,0 +1,13 @@
+package org.fs.logfileanalyzeratweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogfileAnalyzerAtWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogfileAnalyzerAtWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fs/logfileanalyzeratweb/LogfileAnalyzerAtWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fs.logfileanalyzeratweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogfileAnalyzerAtWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
