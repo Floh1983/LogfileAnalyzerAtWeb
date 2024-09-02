50052f59259827b910a95fd34e2d78637cb4c0af
Floh1983 <florian.schlosser@comline.de>
2024-09-02 12:35:32 +0200

    LogfileAnalyzerAtWebApplication

--- src/main/java/org/fs/logfileanalyzeratweb/LogfileAnalyzerAtWebApplication.java
+++ src/main/java/org/fs/logfileanalyzeratweb/LogfileAnalyzerAtWebApplication.java
@@ -3,7 +3,10 @@ package org.fs.logfileanalyzeratweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 
 @SpringBootApplication
@@ -12,6 +15,8 @@ public class LogfileAnalyzerAtWebApplication {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(LogfileAnalyzerAtWebApplication.class, args);
 		Textsearch mytext = new Textsearch();
-		mytext.textsearch();
+		mytext.textsearch("C:\\Users\\u1166832\\Documents\\Praktikum\\IHK\\Projekt\\Test.txt",
+				"ante",
+				"C:\\Users\\u1166832\\Documents\\Praktikum\\IHK\\Projekt\\TestAusgabe.txt");
 	}
 }
--- src/main/java/org/fs/logfileanalyzeratweb/Textsearch.java
+++ src/main/java/org/fs/logfileanalyzeratweb/Textsearch.java
@@ -1,26 +1,43 @@
 package org.fs.logfileanalyzeratweb;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@Component
+@Service
 public class Textsearch {
 
-    public void textsearch() throws IOException {
-        //Übergabe des Dateipfades
-        Path path = Paths.get("C:\\Users\\u1166832\\Documents\\Praktikum\\IHK\\Projekt\\Test.txt");
-        //Auslesen Der Textdatei
-        String line;
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
-            line = reader.readLine();
-        }
-        //Ausgabe der ersten Zeile
-        System.out.println(line);
+//    public void textsearch() throws IOException {
+//        //Übergabe des Dateipfades
+//        Path path = Paths.get("C:\\Users\\u1166832\\Documents\\Praktikum\\IHK\\Projekt\\Test.txt");
+//        //Auslesen Der Textdatei
+//        String line;
+//        try (BufferedReader reader = Files.newBufferedReader(path)) {
+//            line = reader.readLine();
+//        }
+//        //Ausgabe der ersten Zeile
+//        System.out.println(line);
+//    }
+
+    public void textsearch(String inputFile, @NotNull String searchText, String outputFile) throws IOException {
+        //Suchtext zu Kleinschreibung
+        String lowerCase = searchText.toLowerCase();
+        //Lesen der .txt
+        List<String> matchingLines = Files.lines(Paths.get(inputFile))
+                //Filtern
+                .filter(line -> line.toLowerCase().contains(lowerCase))
+                //Bauen der Liste
+                .collect(Collectors.toList());
+        //Ausgeben der Liste als .txt
+        Files.write(Paths.get(outputFile), matchingLines);
     }
 }
 
