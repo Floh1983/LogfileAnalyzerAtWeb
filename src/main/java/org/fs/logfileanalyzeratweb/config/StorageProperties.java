8906946c60211e117fac8d2827f7798211cc409f
yumelong <yumefuchs@gmail.com>
2024-10-17 13:46:16 +0200

    Textsuche

--- src/main/java/org/fs/logfileanalyzeratweb/config/StorageProperties.java
+++ src/main/java/org/fs/logfileanalyzeratweb/config/StorageProperties.java
@@ -2,15 +2,13 @@ package org.fs.logfileanalyzeratweb.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Data
 public class StorageProperties {
 
-
+    //Festlegen des Spreicherortes für den StorageService
     @Value("${storage.location}")
     public String location;
 
